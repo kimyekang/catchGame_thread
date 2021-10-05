802931ad0c4fbd9066b91a9b9f736ddc941cbf8f
kimyekang <gongg10@naver.com>
2021-10-06 03:07:12 +0900

    Not yet

---
+++ app/src/main/java/com/pioneerapp/catchgame_thread/CatchActivity.kt
@@ -0,0 +1,11 @@
+package com.pioneerapp.catchgame_thread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CatchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catch)
+    }
+}
--- app/src/main/java/com/pioneerapp/catchgame_thread/MainActivity.kt
+++ app/src/main/java/com/pioneerapp/catchgame_thread/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.pioneerapp.catchgame_thread
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startGame(view: View?) {
+        val intent = Intent(this, CatchActivity::class.java)
+        startActivity(intent)
     }
 }
